8073defd607652f9b69013c7f63593f37b291d36
Javier Carceles Fernandez <jcarceles25@politecnics.barcelona>
2026-07-02 18:51:24 +0200

    Cambios de saber las 3 maneras de comunicarse con el usuario

---
+++ EX1/script.js
@@ -0,0 +1,4 @@
+// alert('Que hace esto aqui?') -> Aparece el pop up con el mensaje
+// console.log('Muchas gracias majo guapeton') -> Escribe en consola del navegador
+// document.write('Muchas gracias majete') -> Escribe en el documento
+
